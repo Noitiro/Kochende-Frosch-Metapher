015720a4e14e86f40dbcb6d1f37a0d7024d5974e
LargeGarbageModel <s33535@pjwstk.edu.pl>
2026-04-25 19:13:14 +0200

    Add resolution logic to EndingCard and extend font assets to support new text elements

--- Assets/Scripts/EndingCard.cs
+++ Assets/Scripts/EndingCard.cs
@@ -8,8 +8,10 @@ public class EndingCard : MonoBehaviour
     private float finalScore;
     private char grade;
     private int citizenNo;
+    private string resolution;
     [SerializeField] private TextMeshProUGUI CitizenNumber;
     [SerializeField] private TextMeshProUGUI PlayerScore;
+    [SerializeField] private TextMeshProUGUI Resolution;
     [SerializeField] private TextMeshProUGUI ScoreGrade;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,29 +28,46 @@ public class EndingCard : MonoBehaviour
         grade = getGradeFromScore();
         int rounded = (int)MathF.Round(finalScore); 
         PlayerScore.text = rounded.ToString();
+        resolution = getResolution();
+        Resolution.text = resolution;
     }
 
     char getGradeFromScore()
     {
-        if (finalScore <= 16.66)
+        if (finalScore <= 16.66f)
         {
             return 'F';
-        }else if (finalScore <= 33.33)
+        }else if (finalScore <= 33.33f)
         {
             return 'E';
-        }else if (finalScore <= 50)
+        }else if (finalScore <= 50f)
         {
             return 'D';
-        }else if (finalScore <= 66.66)
+        }else if (finalScore <= 66.66f)
         {
             return 'C';
-        }else if (finalScore <= 83.33)
+        }else if (finalScore <= 83.33f)
         {
             return 'B';
         }
         else
         {
             return 'A';
+        }
+    }
+
+    String getResolution()
+    {
+        if (finalScore <= 33f)
+        {
+            return "EXECUTE";
+        }else if (finalScore <= 66f)
+        {
+            return "REHABILITATE";
+        }
+        else
+        {
+            return "EXTEND TESTS";
         }
     }
 
