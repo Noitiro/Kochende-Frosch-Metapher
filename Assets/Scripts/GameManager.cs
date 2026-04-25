125e7cb13a68f8f2fa96f65c0ac0569d242aa1d4
Noitiro <157133387+Noitiro@users.noreply.github.com>
2026-04-25 17:25:58 +0200

    add script gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    [SerializeField] private int numberOfDays;
+    public int counterMinigame;
+    void Start(){
+        
+    }
+    void Update(){
+        if(counterMinigame <= numberOfDays){
+
+        }
+        else{
+            
+        }
+    }
+}
