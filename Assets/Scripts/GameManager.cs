4c191ad6b6d8fcb58071eced00af17aefbc61d3c
Noitiro <157133387+Noitiro@users.noreply.github.com>
2026-04-25 18:06:27 +0200

    Add TV and path Grandma

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour{
     [SerializeField] private int numberOfDays;
+    [SerializeField] private Collider2D tv;
     public int counterMinigame;
     void Start(){
-        
+        counterMinigame = 1;
     }
     void Update(){
-        if(counterMinigame <= numberOfDays){
-
+        if(counterMinigame <= numberOfDays)
+        {
+            counterMinigame++;
         }
         else{
-            
+            //end game
         }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.name == "Tv") {
+            startTv();
+        }
+    }
+    void startTv()
+    {
+        Debug.Log ("Start TV minigame");
     }
 }
