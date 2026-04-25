59d190b156338ff1ad525528e4ee530e5e31d45b
LargeGarbageModel <s33535@pjwstk.edu.pl>
2026-04-25 16:40:59 +0200

    Progress bar fully implemented with a demo score set for the time being

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -1,27 +1,46 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float fillSpeed = 1.5f;
+
     float currentProgress;
     float maxProgress;
     float multiplier;
+
     void Start()
     {
-        currentProgress = 0;
+        currentProgress = 50;
         maxProgress = 100;
         multiplier = 1;
+
+        if (fillImage != null)
+            fillImage.fillAmount = 0f;
     }
 
     void Update()
     {
-        
+        // Prevent going out of bounds
+        currentProgress = Mathf.Clamp(currentProgress, 0, maxProgress);
+
+        float targetFill = currentProgress / maxProgress;
+
+        // Smoothly animate fill left -> right at a constant rate
+        if (fillImage != null)
+            fillImage.fillAmount = Mathf.MoveTowards(
+                fillImage.fillAmount,
+                targetFill,
+                fillSpeed * Time.deltaTime
+            );
     }
-    
+
     float getProgress()
     {
         return currentProgress;
     }
+
     void gameResultHandler(float pointAmmount)
     {
         pointAmmount /= 10;
@@ -30,7 +49,8 @@ public class ProgressBar : MonoBehaviour
             pointAmmount *= multiplier;
             currentProgress -= pointAmmount;
             multiplier = 1;
-        }else if (pointAmmount <= 66)
+        }
+        else if (pointAmmount <= 66)
         {
             currentProgress += pointAmmount;
             multiplier = 1;
@@ -46,12 +66,8 @@ public class ProgressBar : MonoBehaviour
     void randomEventsHandler(bool isNegative)
     {
         if (isNegative)
-        {
             currentProgress -= 5;
-        }
         else
-        {
             currentProgress += 5;
-        }
     }
 }
