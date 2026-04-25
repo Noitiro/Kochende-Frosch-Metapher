ec7e319cda5c45bbb496c8568a65690aa2b58f2d
Noitiro <157133387+Noitiro@users.noreply.github.com>
2026-04-25 18:37:15 +0200

    New map and add random event

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour{
     [SerializeField] private int numberOfDays;
     [SerializeField] private Collider2D tv;
+    [SerializeField] private GameObject tvEvent;
+
     public int counterMinigame;
     void Start(){
         counterMinigame = 1;
@@ -25,5 +27,7 @@ public class GameManager : MonoBehaviour{
     void startTv()
     {
         Debug.Log ("Start TV minigame");
+        tvEvent.GetComponent<EventManger>().StartEventSequence();
+        // tvEvent.EventManger.StartEventSequence;
     }
 }
